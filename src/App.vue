6e37710d61a14cda4a55abe028c4c6b23845f915
tangqiang <3256224359@qq.com>
2023-11-18 17:32:34 +0800

    支持ref，自定义组件

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@
     @submit="onSubmit"
     label-width="100"
     ref="formRef"
+    @validate="show"
   ></CForm>
   <!-- <Normal ref="formRef"></Normal> -->
-  <el-button @click="console.log(formRef)">插入一项</el-button>
+  <el-button @click="getSomething">插入一项</el-button>
 </template>
 
 <script lang="tsx" setup>
@@ -21,28 +22,45 @@ import { ElInput } from "element-plus";
 // import { ElButton } from "element-plus";
 // import { ElInput, ElSwitch } from "element-plus";
 const formRef = ref<any>();
-
+const getSomething = async () => {
+  const result = await formRef.value.formRef.validate();
+  console.log(result);
+};
+const show = () => {
+  console.log("事件被触发，透传成功");
+};
 onMounted(() => {
-  console.log(formRef.value);
+  // console.log(formRef.value.validate);
+  // console.log(formRef.validate);
+  // console.log(formRef.value.validate);
 });
+// console.log(ElInput.name);
 const form = ref<any>({});
 // name字段名，后面提示
 const items = ref({
   // 默认input
   name: "姓名",
-  // input2: {
-  //   inner: "el-input",
-  //   label: "输入2",
-  //   default: "默认输入2",
-  // },
-  // switch1: {
-  //   label: "开关1",
-  //   inner: markRaw(ElInput),
-  //   // inner: markRaw(ElInput),
-  //   // prop: "a[0].b.c",
-  //   prop: ["a", "c", "b", "c"],
-  //   // 如果输入数组，会在被proxys使用前转换为字符串，即['a','b']转换为'a,b'
-  // },
+  input2: {
+    inner: "el-input",
+    label: "输入2",
+    default: "默认输入2",
+  },
+  switch1: {
+    label: "开关1",
+    inner: {
+      is: "el-switch",
+      event: {
+        blur: () => {
+          console.log("失去焦点");
+        },
+      },
+    },
+    // slot:true,
+    // inner: markRaw(ElInput),
+    // prop: "a[0].b.c",
+    prop: ["a", "c", "b", "c"],
+    // 如果输入数组，会在被proxys使用前转换为字符串，即['a','b']转换为'a,b'
+  },
   // switch2: {
   //   label: "开关2",
   //   inner: {
--- src/CForm.tsx
+++ src/CForm.tsx
@@ -1,9 +1,6 @@
 
 import { defineComponent, ref, onMounted, onBeforeUpdate, markRaw, h, resolveComponent, defineExpose, toRaw } from 'vue'
-import { get, set, toPairs, omit } from 'lodash-es'
-
-// const myInput = h(resolveComponent('el-input'))
-
+import { get, set, toPairs, omit, mapKeys, startCase, kebabCase } from 'lodash-es'
 
 export default defineComponent({
   props: {
@@ -17,154 +14,147 @@ export default defineComponent({
     const onSubmit = () => {
       console.log(form, prop.form);
       emit('submit')
-      console.log(normalRef)
     }
 
-
+    const formItemRef = {}
     const newFormItem = ref({});
-    const genNewFormItem = () => {
-      newFormItem.value = { ...prop.formItem };
-      const keys = Object.keys(prop.formItem);
-      for (let key of keys) {
-        let column = prop.formItem[key];
-        // 预处理
-        if (typeof column === "string") {
-          Reflect.set(newFormItem.value, key, {
-            key: key,
-            prop: key,
-            label: column,
-            inner: { is: h(resolveComponent("el-input")) },
-            ref: ref({})
+    newFormItem.value = { ...prop.formItem };
+    const keys = Object.keys(prop.formItem);
+
+    for (let key of keys) {
+      Reflect.set(formItemRef, key, ref())
+      let column = prop.formItem[key];
+      // 预处理
+      if (typeof column === "string") {
+        Reflect.set(newFormItem.value, key, {
+          key: key,
+          prop: key,
+          label: column,
+          inner: { is: 'el-input' },
+        });
+      }
+      if (typeof column === "object") {
+        // prop.formItem[key] = toRaw(column)
+        // 设置默认属性，可以直接使用lodash的defaults
+        if (!Reflect.has(column, "key")) {
+          Reflect.set(column, "key", key);
+        }
+        if (!Reflect.has(column, "label")) {
+          Reflect.set(column, "label", key);
+        }
+        if (!Reflect.has(column, "prop")) {
+          Reflect.set(column, "prop", key);
+        } else if (Array.isArray(column['prop'])) {
+          column['prop'] = column['prop'].reduce((pre, cur) => {
+            if (isNaN(Number(cur))) {
+              return pre + "." + cur;
+            } else {
+              return pre + "[" + cur + "]";
+            }
           });
         }
-        if (typeof column === "object") {
-          // 设置默认属性，可以直接使用lodash的defaults
-          if (!Reflect.has(column, "key")) {
-            Reflect.set(column, "key", key);
-          }
-          if (!Reflect.has(column, "label")) {
-            Reflect.set(column, "label", key);
-          }
-          if (!Reflect.has(column, "ref")) {
-            // Reflect.set(column, "ref", ref());
-            column.ref = ref<any>()
-          }
-          if (!Reflect.has(column, "prop")) {
-            Reflect.set(column, "prop", key);
-          } else if (Array.isArray(column['prop'])) {
-            // 传入数组表示path需要处理
-            // TODO 格式检查，比如第一个字母不能是
-            try {
-              // 让get去检查格式问题并报错。但是试过了没有反应
-              get(form, column['prop'])
-            } catch (e) {
-              console.log(e);
-            }
-            column['prop'] = column['prop'].reduce((pre, cur) => {
-              if (isNaN(Number(cur))) {
-                return pre + "." + cur;
+        if (Reflect.has(column, "inner")) {
+          const comp = Reflect.get(column, "inner");
+          if (typeof comp === "string") {
+            // {inner:string}
+            Reflect.set(column, "inner", { is: comp });
+          } else if (typeof comp === "object" && comp) {
+            // comp就是column['inner']
+
+
+            if (Reflect.has(comp, "is")) {
+              // {inner:{is:xxx}}
+              if (typeof Reflect.get(comp, "is") === "string") {
               } else {
-                return pre + "[" + cur + "]";
+                // 对组件进行处理
+                Reflect.set(column, "inner", {
+                  ...comp,
+                  is: kebabCase(comp.name),
+                });
               }
-            });
-          }
-          if (Reflect.has(column, "inner")) {
-            const comp = Reflect.get(column, "inner");
-            if (typeof comp === "string") {
-              Reflect.set(column, "inner", { is: h(resolveComponent(comp)) });
-            } else if (typeof comp === "object" && comp) {
-              // comp就是column['inner']
-              if (Reflect.has(comp, "is")) {
-                if (typeof Reflect.get(comp, "is") === "string") {
-                } else {
-                  // 对组件进行markRaw
-                  Reflect.set(column, "inner", {
-                    ...comp,
-                    is: markRaw(comp.is),
-                  });
-                }
+            } else {
+              if (Reflect.has(comp, 'name')) {
+                // {inner:组件}
+                // 对组件进行处理
+                Reflect.set(column, "inner", { is: kebabCase(comp.name) });
               } else {
-                // 对组件进行markRaw
-                Reflect.set(column, "inner", { is: markRaw(comp) });
+                // {inner:{ 没有is }}，默认is
+                Reflect.set(column, 'inner', { is: 'el-input' })
               }
             }
+
+            if (Reflect.has(comp, 'event')) {
+              Reflect.set(comp, 'event', mapKeys(comp['event'], (value, key) => 'on' + startCase(key)))
+            } else {
+              Reflect.set(comp, 'event', {})
+            }
           }
-          //
-          // 处理插槽
-          // const slotKeys = Object.keys(slots);
-          // for (let key of slotKeys) {
-          //   const res = key.match(/^(\S+)-(\S+)/);
-          //   // 查找不到则res为null
-          //   if (res && res[2] == Reflect.get(column, "key")) {
-          //     if (!Reflect.has(column, "slot")) {
-          //       Reflect.set(column, "slot", {});
-          //     }
-          //     Reflect.set(Reflect.get(column, "slot"), res[1], res[0]);
-          //   }
-          // }
-          // 直接对form进行修改，赋初始值，在保证有prop后进行。prop标识属性在form中的位置
-          if (Reflect.has(column, "default")) {
-            // prop:string / string[]
-            set(prop.form, column.prop, column.default);
-          }
+        } else {
+          set(column, "inner.is", 'el-input')
+          set(column, "inner.event", {})
+          // Reflect.set(column, "inner", { is: comp });
+        }
+        //
+        // 处理插槽
+        // const slotKeys = Object.keys(slots);
+        // for (let key of slotKeys) {
+        //   const res = key.match(/^(\S+)-(\S+)/);
+        //   // 查找不到则res为null
+        //   if (res && res[2] == Reflect.get(column, "key")) {
+        //     if (!Reflect.has(column, "slot")) {
+        //       Reflect.set(column, "slot", {});
+        //     }
+        //     Reflect.set(Reflect.get(column, "slot"), res[1], res[0]);
+        //   }
+        // }
+        // 直接对form进行修改，赋初始值，在保证有prop后进行。prop标识属性在form中的位置
+        if (Reflect.has(column, "default")) {
+          set(prop.form, column.prop, column.default);
         }
       }
+    }
+    // };
+    // genNewFormItem()
 
-      console.log(newFormItem.value.name);
-      // toPairs(newFormItem.value).forEach(([key, value]) => {
-      //   console.log(value.inner.is)
-      // })
-    };
+    console.log(newFormItem)
 
-    // TODO 无法处理多层嵌套
-    // watch不生效
-    // watch(() => prop.formItem, genNewFormItem, { immediate: true });
-    onMounted(genNewFormItem);
-    onBeforeUpdate(genNewFormItem);
+
 
     const proxys = new Proxy(form, {
       get: function (target, property) {
-        // function返回值用于读取
-        // console.log(target)
-        // console.log(form)
         return get(target, property)
-        // 如果是数组怎么办？预处理时将其转换为字符串
       },
       set: function (target, property, value) {
-        // console.log(value)
         set(target, property, value)
         return true;
       }
     })
 
-    let normalRef = {}
-    onMounted(() => {
-      console.log(formRef.value.validate())
-      normalRef.son = ref()
-    })
 
+    // 暴露ref、事件，可以使用proxy优化这个过程
+    const formRef = ref({})
+    expose({ formRef, formItemRef })
 
-    // 暴露事件
-    const formRef = ref<any>()
-    expose({
-      validate: () => formRef.value.validate()
-      // 自行添加。。。
-    })
     return () => <>
       <el-form model={form} ref={formRef} {...attrs}>
         {
           toPairs(newFormItem.value).map(([key, value]) => {
             return <>
-              <el-form-item {...omit(value, ['default', 'inner', 'prop'])} ref={normalRef.son}>
-                {/* <myInput v-model={proxy[value.prop]}></myInput> */}
-                {/* {myInput('hhh')} */}
-                {/* {h(resolveComponent('el-input'))} */}
-
-                {/* {h(value.inner.is, { 'v-model': proxys[value.prop] })} */}
-                {/* {h(resolveComponent('el-input'), { 'v-model': proxy[value.prop] }, '按钮')} */}
-                <el-input v-model={proxys[value.prop]} {...omit(value.inner, ['is'])}></el-input>
-                {/* 批量书写也可以 */}
-              </el-form-item></>
+              <el-form-item {...omit(value, ['default', 'inner', 'prop'])} ref={formItemRef[key]}>
+                {
+                  value.inner.is === 'el-input' && <el-input v-model={proxys[value.prop]} {...omit(value.inner, ['is', 'event'])} {...value.inner.event}></el-input>
+                }
+                {
+                  value.inner.is === 'el-switch' && <el-switch v-model={proxys[value.prop]} {...omit(value.inner, ['is', 'event'])} {...value.inner.event}></el-switch>
+                }
+                {
+                  value.inner.is === 'el-rate' && <el-rate v-model={proxys[value.prop]} {...omit(value.inner, ['is', 'event'])} {...value.inner.event}></el-rate>
+                }
+                {
+                  value.inner.is === 'el-color-picker' && <el-color-picker v-model={proxys[value.prop]} {...omit(value.inner, ['is', 'event'])} {...value.inner.event}></el-color-picker>
+                }
+              </el-form-item>
+            </>
           })
         }
         <el-button onClick={onSubmit}>tijiao</el-button>
